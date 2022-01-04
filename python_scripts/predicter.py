54f8de3e38cfda392fe3d90dee981b1f04f14650
dbezliudnov <dbezliudnov@foundationmedicine.com>
2022-01-04 19:12:00 +0200

    add sorting and predict all data

--- python_scripts/predicter.py
+++ python_scripts/predicter.py
@@ -6,10 +6,10 @@ from names.model import to_dict
 def predict(model: Model, prediction_data):
     res = model.predict(to_dict(prediction_data))
 
-    result = [(i, x[0]) if x > 0.5 else (-1, -1)
+    result = [(i, x[0]) if x > 0.65 else (-1, -1)
               for i, x in enumerate(res)]
     result = list(result | where(lambda x: x[0] >= 0))
-
+    result.sort(key=lambda x: x[1], reverse=True)
     l_res = len(result)
 
     # VISUALIZATION
--- python_scripts/runner.py
+++ python_scripts/runner.py
@@ -3,8 +3,6 @@ import names.data_reader_csv as data_reader
 import names.data_preparer as data_preparer
 from names import model
 from predicter import predict
-from os import linesep
-from functools import reduce
 
 (names, genders, dates) = data_reader.get_data(
     config.root_dir + '\\data\\names.csv')
@@ -15,16 +13,18 @@ history = model.train_model(my_model, train_data, 80)
 
 normalized_data = data_preparer.normalize_merge_data(
     names, len(names[0]), genders, dates)
-prediction_data = list(map(
-    lambda x: [normalized_data[0][0],
-               normalized_data[0][1],
-               normalized_data[0][2],
-               x[0], x[1], x[2]],
-    normalized_data))
 
-indicies = predict(my_model, prediction_data)
-dups = list(map(
-    lambda i: f'{i[1]:.2} : {names[0]}-{genders[0]}-{dates[0]} | {names[i[0]]}-{genders[i[0]]}-{dates[i[0]]}',
-    indicies))
-print('\n'.join(dups))
+for i, x in enumerate(normalized_data):
+    prediction_data = list(map(
+        lambda x: [normalized_data[i][0],
+                normalized_data[i][1],
+                normalized_data[i][2],
+                x[0], x[1], x[2]],
+        normalized_data))
+
+    indicies = predict(my_model, prediction_data)
+    dups = list(map(
+        lambda j: f'{j[1]:.2} : {names[i]}-{genders[i]}-{dates[i]} | {names[j[0]]}-{genders[j[0]]}-{dates[j[0]]}',
+        indicies))
+    print('\n'.join(dups))
 # model.print_history(history)
